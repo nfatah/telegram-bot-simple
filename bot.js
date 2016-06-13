d5c158e66b1eb6fe395f7a5a8ff7ed041f9fa096
Negus <nfatah41@gmail.com>
2016-06-13 06:45:38 +0300

    use webhooks for heroku sleep

--- bot.js
+++ bot.js
@@ -1,13 +1,35 @@
-var token = '207191660:AAFVM7rhrhSFBnTIRZkUnjbj6knJHYibAEU';
+var token = process.env.TOKEN;
 
-var Bot = require('node-telegram-bot-api'),
-    bot = new Bot(token, { polling: true });
+var Bot = require('node-telegram-bot-api');
+var bot;
+
+if(process.env.NODE_ENV === 'production') {
+  bot = new Bot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+}
+else {
+  bot = new Bot(token, { polling: true });
+}
 
 console.log('bot server started...');
 
+// hello command
 bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
   var name = match[1];
   bot.sendMessage(msg.chat.id, 'Hello ' + name + '!').then(function () {
     // reply sent!
   });
 });
+
+// sum command
+bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
+  var result = 0;
+  match[1].trim().split(/\s+/).forEach(function (i) {
+    result += (+i || 0);
+  })
+  bot.sendMessage(msg.chat.id, result).then(function () {
+    // reply sent!
+  });
+});
+
+module.exports = bot;
